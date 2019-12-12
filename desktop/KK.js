0c73e2bc7e3bb35ba3e3b01aa4d070f01b1d0784
nikolay.soldatov <nikolay.soldatov@nevosoft.ru>
2019-12-12 18:36:53 +0300

    deploy

--- desktop/KK.js
+++ desktop/KK.js
@@ -67361,7 +67361,7 @@ var lime_utils_AssetCache = function() {
 	this.audio = new haxe_ds_StringMap();
 	this.font = new haxe_ds_StringMap();
 	this.image = new haxe_ds_StringMap();
-	this.version = 957831;
+	this.version = 899297;
 };
 $hxClasses["lime.utils.AssetCache"] = lime_utils_AssetCache;
 lime_utils_AssetCache.__name__ = ["lime","utils","AssetCache"];
